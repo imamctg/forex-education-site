df70b94f2568ce2a1b195e593871fb69cf2079f6
imamctg <im788788@gmail.com>
2021-10-05 12:16:11 +0600

    header text hover change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationBar from '../Navbar/NavigationBar';
+import './Header.css'
 
 
 /* This is Header component part of the site also link navigarion bar from here */
@@ -8,7 +9,7 @@ const Header = () => {
         <div>
             <div className="mt-3">
                 <h2 className="text-danger fw-bold">Trading Spot</h2>
-                <p>World Best  Forex Trading Training in <span className=" fw-bold text-success">Trading Spot</span></p>
+                <p>World Best  Forex Trading Training in <span className="site fw-bold">Trading Spot</span></p>
             </div>
             <NavigationBar></NavigationBar>
         </div>
